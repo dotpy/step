1f13db5038111bc0a71329d8e9f0c12d7a9bfa13
Daniele Mazzocchio <danix@kernel-panic.it>
2013-11-19 00:26:22 +0100

    PEP8 compliance
    Removed a few bank lines for closer compliance to PEP8.

--- step/step.py
+++ step/step.py
@@ -9,7 +9,6 @@ class Template(object):
     # Regex for stripping all leading, trailing and interleaving whitespace.
     RE_STRIP = re.compile("(^[ \t]+|[ \t]+$|(?<=[ \t])[ \t]+|^\r?\n)", re.M)
 
-
     def __init__(self, template, strip=True):
         """Initialize class"""
         super(Template, self).__init__()
@@ -23,7 +22,6 @@ class Template(object):
             self.code = self._process(self._preprocess(self.template))
             Template.COMPILED_TEMPLATES[template] = self.code
 
-
     def expand(self, namespace={}, **kw):
         """Return the expanded template string"""
         output = []
@@ -33,7 +31,6 @@ class Template(object):
 
         eval(compile(self.code, "<string>", "exec"), namespace)
         return self._postprocess("".join(map(to_unicode, output)))
-
 
     def stream(self, buffer, namespace={}, encoding="utf-8", **kw):
         """Expand the template and stream it to a file-like buffer."""
@@ -55,7 +52,6 @@ class Template(object):
         eval(compile(self.code, "<string>", "exec"), namespace)
         write_buffer("", flush=True) # Flush any last cached bytes
 
-
     def _preprocess(self, template):
         """Modify template string before code conversion"""
         # Replace inline ('%') blocks for easier parsing
@@ -68,7 +64,6 @@ class Template(object):
         template = v.sub(r"<%echo(\g<1>)%>\n", template)
 
         return template
-
 
     def _process(self, template):
         """Return the code generated from the template string"""
@@ -107,7 +102,6 @@ class Template(object):
             raise EOFError("Line {0}: {1}".format(n, err))
 
         return "\n".join(code)
-
 
     def _postprocess(self, output):
         """Modify output string after variables and code evaluation"""
