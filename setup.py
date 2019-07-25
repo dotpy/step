9d6fd7e7f56fa383e9ca52275d91020cb56f8b57
taojy123 <taojy123@163.com>
2019-07-25 14:05:15 +0800

    improve code and fix issue#5

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ from step.tests import TestCommand
 
 
 __author__ = "Daniele Mazzocchio <danix@kernel-panic.it>"
-__version__ = "0.0.2"
-__date__    = "Nov 18, 2013"
+__version__ = "0.0.3"
+__date__    = "Jul 25, 2019"
 
 
 # Python versions prior 2.2.3 don't support 'classifiers' and 'download_url'
